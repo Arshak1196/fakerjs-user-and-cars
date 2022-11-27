302dc30f1d6aea0c84c7069c24d05c6bb2e06ba8
Arshak <arshakmohammed004@gmail.com>
2022-11-28 02:17:15 +0530

    filter in piegraph

--- src/Pages/CarPage.js
+++ src/Pages/CarPage.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
+function CarPage(props) {
+  const [users,setUsers]=useState([])
+  const carMakersLabel = [...new Set(users.map((user) => user?.vehicle.manufacturer))].sort()
+  const carAgeLabel = [...new Set(users.map((user) => user?.vehicle.age))].sort()
 
-
-function CarPage({ users }) {
-
-  const carMakersLabel = [...new Set(users.map((user) => user.vehicle.manufacturer))].sort()
-  const carAgeLabel = [...new Set(users.map((user) => user.vehicle.age))].sort()
+  useEffect(()=>{
+    setUsers(props.users)
+  },[props.users])
 
   const carMakersData = {
     labels: carMakersLabel,
@@ -18,7 +20,7 @@ function CarPage({ users }) {
         label: '#Cars',
         data: carMakersLabel.map((manufacturer) => {
           return (users.reduce((acc, curr) => {
-            if (curr.vehicle.manufacturer === manufacturer) {
+            if (curr?.vehicle.manufacturer === manufacturer) {
               acc++
             }
             return acc
@@ -42,7 +44,7 @@ function CarPage({ users }) {
         label: '#Cars',
         data: carAgeLabel.map((age) => {
           return (users.reduce((acc, curr) => {
-            if (curr.vehicle.age === age) {
+            if (curr?.vehicle.age === age) {
               acc++
             }
             return acc
@@ -59,20 +61,49 @@ function CarPage({ users }) {
     ],
   }
 
-
-  console.log(users)
-  console.log(carAgeLabel)
+  const handlefilter=(e)=>{
+    if(e.target.value==='all'){
+      setUsers(props.users)
+    }else if(e.target.value==='50'){
+      setUsers(props.users.filter((user)=>{
+        if(user.age>=50){
+          return user
+        }
+      }))
+    }else{
+      const value=Number(e.target.value)
+      setUsers(props.users.filter((user)=>{
+        if(user.age>=value && user.age<(value+10)){
+          return user
+        }
+      }))
+    }
+  }
+  
   return (
     <div className='main-div'>
       <div className='ageFiter'>
-        <input type='radio' name='age'/>
+        <h2>Piegraph for Cars</h2>
+        <form onChange={handlefilter}>
+          <input id="personal-account" type="radio" value="all" name="age" className="inline" defaultChecked /> All Users
+          <input id="personal-account" type="radio" value="20" name="age" className="inline" /> 20-30
+          <input id="personal-account" type="radio" value="30" name="age" className="inline" /> 31-40
+          <input id="personal-account" type="radio" value="40" name="age" className="inline" /> 41-50
+          <input id="personal-account" type="radio" value="50" name="age" className="inline" /> 51-60
+        </form>
+        <p>Total Users: {users.length}</p>
       </div>
-      <div>
-        <Pie data={carMakersData} />
+      <div className='piegraph'>
+        <div className='sub-div'>
+          <h3>Owners of Each company</h3>
+          <Pie data={carMakersData} />
+        </div>
+        <div className='sub-div'>
+          <h3>Average age of cars</h3>
+          <Pie data={carAgeData} />
+        </div>
       </div>
-      <div>
-        <Pie data={carAgeData} />
-      </div>
+
     </div>
   )
 }
